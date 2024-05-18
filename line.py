6f8750ba4e088bc9850b0790d9859f9c376b25c3
Cesar Augusto <cesaraugustoandradeferreira@gmail.com>
2024-05-18 12:55:47 -0300

    lidando com todos os casos para as linhas

--- line.py
+++ line.py
@@ -17,11 +17,44 @@ def drawline(x0, y0, x1, y1, cor):
     Dy = y1 - y0
     Dx = x1 - x0
     print(f"Delta x: {Dx} | Delta y: {Dy}")
-    a = Dy / Dx
-    y = y0
-    for x in range(x0, x1+1):
-        putpixel(x, y, (cor))
-        y = y + a
+
+    if abs(Dx) > abs(Dy):
+
+        # garantindo que X0 é menor que X1
+        tx0, tx1 = 0, 0
+
+        if x0 > x1:
+            tx0 = x0
+            tx1 = x1
+            x1 = tx0
+            x0 = tx1
+
+        print(f"X0 {x0}, X1 {x1}")
+
+        a = Dy / Dx
+        y = y0
+        for x in range(x0, x1+1):
+            putpixel(x, y, (cor))
+            y = y + a
+
+    else:
+
+        # garantindo que Y0 é menor que Y1
+        ty0, ty1 = 0, 0
+
+        if y0 > y1:
+            ty0 = y0
+            ty1 = y1
+            y1 = ty0
+            y0 = ty1
+
+        print(f"y0 {y0}, y1 {y1}")
+
+        a = Dx / Dy
+        x = x0
+        for y in range(y0, y1+1):
+            putpixel(x, y, (cor))
+            x = x + a
 
 exit = False
   
@@ -29,9 +62,7 @@ while not exit:
 
     canvas.fill((255, 255, 255))
 
-    drawline(0, 0, 240, 120, (0, 0, 0))
-    drawline(0, 0, 60, 240, (0, 0, 0))
-    drawline(60, 240, 240, 120, (0, 0, 0))
+    drawline(20, 120, 100, 240, (0, 0, 0))
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
